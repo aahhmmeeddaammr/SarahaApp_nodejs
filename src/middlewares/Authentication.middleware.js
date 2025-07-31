16240a9520da8cdfe6c4ccf3fe3412e5f38c8ba8
aahhmmeeddaammr <aamr24987@gmail.com>
2025-08-01 00:38:51 +0300

    Debug

--- src/middlewares/Authentication.middleware.js
+++ src/middlewares/Authentication.middleware.js
@@ -4,6 +4,7 @@ import { decodeToken, tokenTypeEnum } from "../utils/security/token.security.js"
 export const Authentication = ({ tokenType = tokenTypeEnum.access } = {}) => {
   return asyncHandler(async (req, res, next) => {
     const authHeader = tokenType == tokenTypeEnum.access ? req.headers.authorization : `Bearer ${req.cookies.refreshToken}`;
+    console.log({ authHeader });
     req.user = await decodeToken({ authorization: authHeader, next, tokenType });
     return next();
   });
